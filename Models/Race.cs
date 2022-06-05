0332210c6044c794d2ac3f04af1d7ee463c51ca8
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-05 22:08:41 +0200

    Adding the Models as the database designed

--- Models/Race.cs
+++ Models/Race.cs
@@ -8,14 +8,13 @@ namespace MotoGP.Models
     {
         public int RaceID { get; set; }
         public string Name { get; set; }
-        public int X { get; set; }
-        public int Y { get; set; }
         public string Country { get; set; }
         public string Description { get; set; }
         public int Length { get; set; }
         public DateTime Date { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
 
-        public Country Countries { get; set; }
         public ICollection<Ticket> Tickets { get; set; }
 
         
