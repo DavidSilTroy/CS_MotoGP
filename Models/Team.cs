40d9186059e3d341695bfc53543310fb32329e7c
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-05 22:18:12 +0200

    ok now I know how to add data with the ID defined and not by defoult

--- Models/Team.cs
+++ Models/Team.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MotoGP.Models
 {
     public class Team
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int TeamID { get;  set; }
         public string Name { get; set; }
         public string Logo { get; set; }
