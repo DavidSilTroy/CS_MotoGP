794f05d9d83b6f12cfd1f4e0dc2f79f50970c5a6
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-05 21:58:09 +0200

    MotorGP from lesson 1 is working perfectly (it seems the database part was later). Whopsis

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace MotoGP.Controllers
             _logger = logger;
         }
 
+        
         public IActionResult Index()
         {
             return View();
--- Controllers/InfoController.cs
+++ Controllers/InfoController.cs
@@ -29,11 +29,11 @@ namespace MotoGp.Controllers
         {
             ViewData["Title"] = "Map";
             ViewData["BannerNr"] = 0;
-            //List<Race> races = new List<Race>();
-            //races.Add( new Race() { RaceID = 1, X = 517, Y = 19, Name = "Assen" });
-            //races.Add( new Race() { RaceID = 2, X = 859, Y = 249, Name = "Losail Circuit" });
-            //races.Add( new Race() { RaceID = 3, X = 194, Y = 428, Name = "Autódromo Termas de Río Hondo" });
-            //ViewData["RacesList"] = races;
+            List<Race> races = new List<Race>();
+            races.Add(new Race() { RaceID = 1, X = 517, Y = 19, Name = "Assen" });
+            races.Add(new Race() { RaceID = 2, X = 859, Y = 249, Name = "Losail Circuit" });
+            races.Add(new Race() { RaceID = 3, X = 194, Y = 428, Name = "Autódromo Termas de Río Hondo" });
+            ViewData["RacesList"] = races;
             return View();
         }
 
