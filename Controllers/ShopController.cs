b5185377803ca0816ac508c9b627bb234155072a
David Silva Troya <davidsiltroy2008@hotmail.com>
2022-06-06 15:21:58 +0200

    Added a select List with the helper tag and the new ModelView. Problem with the controller was solve, I needed to use the same Model as a second parameter

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using MotoGP.Data;
 using MotoGP.Models;
+using MotoGP.Models.ViewModels;
 using System;
 using System.Linq;
 
@@ -26,30 +28,34 @@ namespace MotoGp.Controllers
         {
             ViewData["Title"] = "Order Tickets";
             ViewData["BannerNr"] = 3;
-            //var ticktes = _context.Tickets;
-            return View() ;
+            var listCountries = new CountriesForTicketsViewModel();
+            listCountries.Countries = new SelectList(_context.Countries.OrderBy(c => c.CountryID),"CountryID" ,"Name");
+            return View(listCountries);
         }
         
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult ConfirmOrder([Bind("Name,Email,Address,Number")] Ticket ticket)
+        public IActionResult ConfirmOrder(
+            [Bind("Name,Email,Address,Number")] Ticket ticket, CountriesForTicketsViewModel countrySelect)
         {
             var date = DateTime.UtcNow;
             ViewBag.ticketInfo = "Error";
             ViewData["Title"] = "Confirmation";
             ViewData["CurrentDateConfirm"] = date.ToString("dd-MM-yyyy");
             ViewData["BannerNr"] = 3;
-            ticket.CountryID = 1;
+
+            ticket.CountryID = countrySelect.CountryID;
             ticket.RaceID = 1;
             ticket.OrderDate = date;
             ticket.Paid = true;
-            if (ModelState.IsValid)
+            if (ModelState.IsValid && ticket.CountryID!=0)
             {
                 _context.Add(ticket);
                 _context.SaveChanges();
                 ViewBag.ticketInfo = "Done";    
                 return View();
             }
+            //ViewBag.ticketInfo = "Error: CountryID: " + ticket.CountryID + "Extra: " + countryThing.CountryID;
             return View();
         }
 
---
+++ Models/ViewModels/CountriesForTicketsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MotoGP.Models.ViewModels
+{
+    public class CountriesForTicketsViewModel
+    {
+        public Ticket Ticket { get; set; }
+        public SelectList Countries { get; set; }
+
+        [Display(Name = "Race")]
+        public List<Race> Races { get; set; }
+        [Display(Name = "Country")]
+        public int CountryID { get; set; }
+
+    }
+}
